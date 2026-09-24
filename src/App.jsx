835bb54972c11cc3310a62024fd889cab5037b0c
Laboratório FIAP <labsfiap@fiap.com.br>
2026-09-24 11:22:10 -0300

    feat: Codificando o app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,25 @@
+import {BrowseRouter as Router, Routes, Route} from 'react-router-dom'
+import Header from './components/header'
+import Footer from './components/footer'
+import Home from './pages/home'
+import Contato from './pages/contato'
+import Jogos from './pages/jogos'
+import Error from './pages/error'
+
 const App = () => {
   return (
-    <>App</>
+    <Router>
+      <div className="min-h-screen flex flex-col justify-between bg-[#1A1A1A] pt-4">
+        <Header/>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/contato" element={<Contato />} />
+          <Route path="/jogos" element={<Jogos />} />
+          <Route path="*" element={<Error />} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   )
 }
 
