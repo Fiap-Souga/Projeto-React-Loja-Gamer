326ef7e4b494d7e567b061abded5ba39f4b9f239
Laboratório FIAP <labsfiap@fiap.com.br>
2026-09-24 11:48:40 -0300

    feat: codificando css

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
-import {BrowseRouter as Router, Routes, Route} from 'react-router-dom'
-import Header from './components/header'
-import Footer from './components/footer'
-import Home from './pages/home'
-import Contato from './pages/contato'
-import Jogos from './pages/jogos'
-import Error from './pages/error'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import Home from './pages/Home'
+import Contato from './pages/Contato'
+import Jogos from './pages/Jogos'
+import Error from './pages/Error'
 
 const App = () => {
   return (
     <Router>
       <div className="min-h-screen flex flex-col justify-between bg-[#1A1A1A] pt-4">
-        <Header/>
+        <Header />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/contato" element={<Contato />} />
